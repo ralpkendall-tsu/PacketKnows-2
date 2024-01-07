28e81395dd272f0d743f63c15d2673919868ea6d
Ralp Kendall Abad <rk.abad0134@student.tsu.edu.ph>
2024-01-07 15:17:45 +0800

    Add filtering on Course model

--- activity/admin.py
+++ activity/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Course)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    search_fields = ('name', 'slug',)
+    prepopulated_fields = {'slug': ('name',)}
+
 admin.site.register(ActivityCategory)
 admin.site.register(BaseActivity)
 admin.site.register(Activity)
