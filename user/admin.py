239a0eccbf1d0b550cd641af8b01528029f4e482
Ralp Kendall Abad <rk.abad0134@student.tsu.edu.ph>
2024-01-11 20:54:04 +0800

    Add category filtering on admin panel:user

--- user/admin.py
+++ user/admin.py
@@ -6,6 +6,7 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('category', 'is_active', 'is_staff')
     ordering = ('email',)
 
     fieldsets = (
